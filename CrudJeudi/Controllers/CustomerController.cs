30ea819ee0acc0bf6dbd9f4cbb6866827dd12e25
Sulliivan <qraks-@hotmail.fr>
2020-11-12 11:21:36 +0100

    ajout formulaire

--- CrudJeudi/Controllers/CustomerController.cs
+++ CrudJeudi/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,6 +18,25 @@ namespace CrudJeudi.Controllers
         {
             var customers = db.customer.ToList();
             return View(customers);
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        public ActionResult Customer()
+        {
+
+            return View();
         }
     }
 }
