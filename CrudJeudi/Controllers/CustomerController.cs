506484384fdefd7c6c6fa9b3cf0f9569b89a7b26
Sulliivan <qraks-@hotmail.fr>
2020-11-12 10:31:17 +0100

    creation liste

---
+++ CrudJeudi/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using CrudJeudi.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudJeudi.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // l'objet db est le context 
+        ShopEntities db = new ShopEntities();
+
+        // GET: Customer
+        public ActionResult CustomerList()
+        {
+            var customers = db.customer.ToList();
+            return View(customers);
+        }
+    }
+}
